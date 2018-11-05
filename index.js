5aa043a91cc2845ba266a7d4a3354771c74fadd2
verticalgrain <jamie@verticalgraindesign.com>
2018-11-04 20:28:40 -0800

    updates

--- index.js
+++ index.js
@@ -18,9 +18,28 @@ ref.set({
 	lightzero: true
 });
 
-fs.writeFile("/sys/class/gpio/export", "1013", function(err) {});
-fs.writeFile("/sys/class/gpio/gpio1013/direction", "out", function(err) {});
-fs.writeFile("/sys/class/gpio/gpio1013/value", "1", function(err) {});
+var gpio = require("gpio");
+ 
+// Calling export with a pin number will export that header and return a gpio header instance
+var gpio3 = gpio.export(3, {
+   // When you export a pin, the default direction is out. This allows you to set
+   // the pin value to either LOW or HIGH (3.3V) from your program.
+   direction: gpio.DIRECTION.OUT,
+ 
+   // set the time interval (ms) between each read when watching for value changes
+   // note: this is default to 100, setting value too low will cause high CPU usage
+   interval: 200,
+ 
+   // Due to the asynchronous nature of exporting a header, you may not be able to
+   // read or write to the header right away. Place your logic in this ready
+   // function to guarantee everything will get fired properly
+   ready: function() {
+   }
+});
+
+// fs.writeFile("/sys/class/gpio/export", "1013", function(err) {});
+// fs.writeFile("/sys/class/gpio/gpio1013/direction", "out", function(err) {});
+// fs.writeFile("/sys/class/gpio/gpio1013/value", "1", function(err) {});
 
 
 ref.on("value", function(snapshot) {
@@ -39,9 +58,9 @@ function updateLights(lights) {
     for (var key in lights) {
         if (lights.hasOwnProperty(key)) {
             if(lights[key]) {
-                fs.writeFile("/sys/class/gpio/gpio1013/value", "1", function(err) {});
+                gpio3.set();
             } else {
-                fs.writeFile("/sys/class/gpio/gpio1013/value", "0", function(err) {});
+                gpio3.set(0);
             }
         }
     }
